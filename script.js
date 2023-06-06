3dfaa3e95ca29dc4db7b873bb8996f226a03314b
Thiago Fogaca <thiagofogaca25@gmail.com>
2023-06-05 21:12:00 -0300

    just sketch

---
+++ script.js
@@ -0,0 +1,72 @@
+// Product data (can be obtained from a database or server)
+ var productsData = [
+    { id: 1, name: 'echo dot', price: 10.00, image: 'https://m.media-amazon.com/images/I/51+iQTwDsXL._AC_SX569_.jpg' },
+    { id: 2, name: 'Iphone 14', price: 1000.00, image: 'https://m.media-amazon.com/images/I/61cwywLZR-L._AC_SX679_.jpg' },
+    { id: 3, name: 'Xbox Series S', price: 250.00, image: 'https://m.media-amazon.com/images/I/81Z1xBs6GoL._AC_SX679_.jpg' },
+];
+
+// Function to load products on the page
+function loadProducts() {
+    var productsContainer = document.getElementById('products');
+    
+    for (var i = 0; i < productsData.length; i++) {
+        var product = productsData[i];
+
+        var colleft = document.createElement('div');
+        colleft.classList.add('col');
+
+        var colright = document.createElement('div');
+        colright.classList.add('col');
+
+        var productElement = document.createElement('div');
+        productElement.classList.add('product');
+        productElement.classList.add('row');
+        
+        var divimage = document.createElement('div');
+        divimage.classList.add('divimage');
+        
+        var productImage = document.createElement('img');
+        productImage.src = product.image;
+        
+        var productName = document.createElement('h3');
+        productName.innerText = product.name;
+        
+        var productPrice = document.createElement('p');
+        productPrice.innerText = '$ ' + product.price.toFixed(2);
+        
+        var addToCartButton = document.createElement('button');
+        addToCartButton.innerText = 'Add to Cart';
+        addToCartButton.addEventListener('click', addToCart.bind(null, product));
+        
+        productElement.appendChild(colleft);
+        productElement.appendChild(colright);
+        colleft.appendChild(divimage);
+        divimage.appendChild(productImage);
+        colright.appendChild(productName);
+        colright.appendChild(productPrice);
+        colright.appendChild(addToCartButton);
+        
+        productsContainer.appendChild(productElement);
+    }
+}
+
+// Function to perform checkout (can be implemented later)
+function addToCart(product) {
+    var cartItemsContainer = document.getElementById('cart-items');
+    
+    var cartItem = document.createElement('li');
+    cartItem.innerText = product.name + ' - R$ ' + product.price.toFixed(2);
+    
+    cartItemsContainer.appendChild(cartItem);
+    
+    var cartTotal = document.getElementById('cart-total');
+    var currentTotal = parseFloat(cartTotal.innerText);
+    cartTotal.innerText = (currentTotal + product.price).toFixed(2);
+}
+
+function checkout() {
+    alert('Checkout completed! Thank you for shopping with us.');
+}
+
+// Load the products when the page loads
+window.addEventListener('load', loadProducts);
